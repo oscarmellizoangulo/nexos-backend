3ed3bee55668864bb65a3311df0ae0c78b873b81
Oscar MA <omellizo@gmail.com>
2018-09-23 22:01:53 -0500

    Se crear la raiz nexos-api para todos los servicios

--- src/main/java/com/nexos/backend/api/NexosBackendApplication.java
+++ src/main/java/com/nexos/backend/api/NexosBackendApplication.java
@@ -2,16 +2,12 @@ package com.nexos.backend.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class NexosBackApplication {
+public class NexosBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NexosBackApplication.class, args);
+		SpringApplication.run(NexosBackendApplication.class, args);
 	}
 	
 //	@Bean
